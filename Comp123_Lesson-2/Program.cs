7fad0c05f4db7c08009f0bcc2e812a78f2cac90d
audiotrash112@gmail.com <audiotrash112@gmail.com>
2016-05-13 11:05:09 -0400

    commit

--- Comp123_Lesson-2/Program.cs
+++ Comp123_Lesson-2/Program.cs
@@ -10,6 +10,25 @@ namespace Comp123_Lesson_2
     {
         static void Main(string[] args)
         {
+            GetUsrName();
+        }
+        /* This method reads user name  to console
+         * @Method GetUrsName
+         * @return {string} usrname
+         * 
+         * version 0.2 - Added UsrName
+         
+             */
+
+        public static string GetUsrName()
+        {
+            Console.Write("Please enter user Name :");
+            string usrname = "";
+           usrname= Console.ReadLine();
+            Console.WriteLine("_________");
+            Console.WriteLine("you entered:" + usrname);
+            return usrname;
+
         }
     }
 }
